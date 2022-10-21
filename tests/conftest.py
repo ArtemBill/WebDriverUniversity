f33a5f287230556070e3967ff9e3d174a6aae3a6
Artem <artem.lincoln.bilyk@gmail.com>
2022-10-21 16:12:08 +0300

    removed --no-sandbox option

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ def setup(request):
     options.add_argument("--headless")
     options.add_argument("window-size=1400,1500")
     options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
+    # options.add_argument("--no-sandbox")
     options.add_argument("start-maximized")
     # options.add_argument("enable-automation")
     # options.add_argument("--disable-infobars")
