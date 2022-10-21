5eaf98f1c02a803a30436c30cb9855a42bfd13e5
Artem <artem.lincoln.bilyk@gmail.com>
2022-10-21 16:53:35 +0300

    removed extra options code

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,7 @@ from selenium.webdriver.chrome.options import Options
 def setup(request):
     options = Options()
     options.add_argument("--headless")
-    # options.add_argument("window-size=1400,1500")
-    # options.add_argument("--disable-gpu")
-    # options.add_argument("--no-sandbox")
     options.add_argument("start-maximized")
-    # options.add_argument("enable-automation")
-    # options.add_argument("--disable-infobars")
-    # options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
 
