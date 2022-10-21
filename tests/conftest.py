0aea82d84853a93004d7712f96e5442ed05933dd
Artem <artem.lincoln.bilyk@gmail.com>
2022-10-21 16:15:30 +0300

    removed window-size=1400,1500 option

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.options import Options
 def setup(request):
     options = Options()
     options.add_argument("--headless")
-    options.add_argument("window-size=1400,1500")
+    # options.add_argument("window-size=1400,1500")
     options.add_argument("--disable-gpu")
     # options.add_argument("--no-sandbox")
     options.add_argument("start-maximized")
